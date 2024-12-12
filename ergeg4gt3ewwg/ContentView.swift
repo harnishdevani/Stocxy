5fd029a85d9a4f235d31d0855d091612a2482a30
harnishdevani <harnishdevani03@gmail.com>
2024-12-12 22:32:25 +0100

    Initial Commit

---
+++ ergeg4gt3ewwg/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ergeg4gt3ewwg
+//
+//  Created by Harnish Devani on 12/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ergeg4gt3ewwg/ergeg4gt3ewwgApp.swift
@@ -0,0 +1,17 @@
+//
+//  ergeg4gt3ewwgApp.swift
+//  ergeg4gt3ewwg
+//
+//  Created by Harnish Devani on 12/12/24.
+//
+
+import SwiftUI
+
+@main
+struct ergeg4gt3ewwgApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
